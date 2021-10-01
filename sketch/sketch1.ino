2d8d6395645233b186340f4aaec8723021fac405
esoterictriangle <triangularart@gmail.com>
2021-10-01 16:55:30 -0500

    working on scanning matrix using shift register it's... very messy rn. things are broken. don't look

--- sketch/sketch1.ino
+++ sketch/sketch1.ino
@@ -27,3 +27,17 @@ void setup() {
 void loop(){
     Control_Surface.loop();
 }
+
+/*
+for (i = 0; i < 8; i++) {
+digitalWrite(mux(i), LOW);
+delay(50); //this is long
+if digitalRead(2) == LOW {
+    digitalWrite()
+}
+}
+
+https://wbsimms.com/use-74hc595-monitor-many-digital-inputs/
+https://tttapa.github.io/Control-Surface-doc/Doxygen/df/d9e/2_8RGB-LED-Chaser_8ino-example.html
+https://forum.arduino.cc/t/74hc595-used-for-shift-input-not-output/160028/3
+*/
