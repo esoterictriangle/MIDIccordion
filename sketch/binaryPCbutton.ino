c7d0c5ad9f7ffc06300b3f90de55958bd0701480
esoterictriangle <triangularart@gmail.com>
2021-10-07 15:55:28 -0500

    added notes on how to achieve the function swap

--- sketch/binaryPCbutton.ino
+++ sketch/binaryPCbutton.ino
@@ -1,4 +1,5 @@
 //basic idea: 1 toggle turns on this mode, remaps all of the pins in question (does this mean those pins need to be assigned in loop or setup instead of globally?)
+//re: (question) https://github.com/tttapa/Control-Surface/issues/308 shows how to disable an array and enable a different array; I think I can use it to disable one version of the toggle matrix an enable a second version
 //1 toggle starts listening for the binary. When all switches are in the correct position, this toggle is flipped back, sending the program change with the value from the binary switches.
   
 //taken from https://tttapa.github.io/Control-Surface-doc/Doxygen/d4/d3b/Custom-MIDI-Output-Element_8ino-example.html
