1fcaeefa2d7983a8480f13c5560ee21c23582c60
esoterictriangle <triangularart@gmail.com>
2022-01-27 11:18:11 -0600

    made note array

--- sketch/muxbutton/muxbutton.ino
+++ sketch/muxbutton/muxbutton.ino
@@ -11,8 +11,17 @@ CD74HC4067 mux {
   // 7, // Optionally, specify the enable pin
 };
 
-NoteButton {
-  
+NoteButton notes[] {
+  {mux.pin(0), {MIDI_Notes::C(4), CHANNEL_1}},
+  {mux.pin(1), {MIDI_Notes::D(4), CHANNEL_1}},
+  {mux.pin(2), {MIDI_Notes::E(4), CHANNEL_1}},
+  {mux.pin(3), {MIDI_Notes::F(4), CHANNEL_1}},
+  {mux.pin(4), {MIDI_Notes::G(4), CHANNEL_1}},
+  {mux.pin(5), {MIDI_Notes::A(4), CHANNEL_1}},
+  {mux.pin(6), {MIDI_Notes::B(4), CHANNEL_1}},
+  {mux.pin(7), {MIDI_Notes::C(5), CHANNEL_1}},
+  {mux.pin(8), {MIDI_Notes::D(5), CHANNEL_1}},
+  {mux.pin(9), {MIDI_Notes::E(5), CHANNEL_1}},
 };
   
   // Initialize the Control Surface
