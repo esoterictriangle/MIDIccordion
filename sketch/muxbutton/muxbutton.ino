ad0b00d59bf87fe90cca462e007d1af6b49abb8a
esoteric triangle <triangularart@gmail.com>
2022-01-29 18:19:26 -0600

    notebuttons is probably the better choice

--- sketch/muxbutton/muxbutton.ino
+++ sketch/muxbutton/muxbutton.ino
@@ -11,25 +11,25 @@ CD74HC4067 mux {
   // 7, // Optionally, specify the enable pin
 };
 
-NoteButton notes[] {
-  {mux.pin(0), {MIDI_Notes::C(4), CHANNEL_1}},
-  {mux.pin(1), {MIDI_Notes::D(4), CHANNEL_1}},
-  {mux.pin(2), {MIDI_Notes::E(4), CHANNEL_1}},
-  {mux.pin(3), {MIDI_Notes::F(4), CHANNEL_1}},
-  {mux.pin(4), {MIDI_Notes::G(4), CHANNEL_1}},
-  {mux.pin(5), {MIDI_Notes::A(4), CHANNEL_1}},
-  {mux.pin(6), {MIDI_Notes::B(4), CHANNEL_1}},
-  {mux.pin(7), {MIDI_Notes::C(5), CHANNEL_1}},
-  {mux.pin(8), {MIDI_Notes::D(5), CHANNEL_1}},
-  {mux.pin(9), {MIDI_Notes::E(5), CHANNEL_1}},
-};
+/*NoteButton notes[] {
+  {mux.pin(0), {60, CHANNEL_1}},
+  {mux.pin(1), {61, CHANNEL_1}},
+  {mux.pin(2), {62, CHANNEL_1}},
+  {mux.pin(3), {63, CHANNEL_1}},
+  {mux.pin(4), {64, CHANNEL_1}},
+  {mux.pin(5), {65, CHANNEL_1}},
+  {mux.pin(6), {66, CHANNEL_1}},
+  {mux.pin(7), {67, CHANNEL_1}},
+  {mux.pin(8), {68, CHANNEL_1}},
+  {mux.pin(9), {69, CHANNEL_1}},
+};*/
 
-//NoteButtons<10> {
-//  {mux.pin(0), mux.pin(1), mux.pin(2), mux.pin(3), mux.pin(4), mux.pin(5), mux.pin(6), mux.pin(7), mux.pin(8),mux.pin(9)}, //array of pins
-//  {MIDI_Notes::C(4), CHANNEL_1}, //address of first button
-//  1, //increment size
-//  127, //velocity
-//};
+NoteButtons<10> notes {
+ {mux.pin(0), mux.pin(1), mux.pin(2), mux.pin(3), mux.pin(4), mux.pin(5), mux.pin(6), mux.pin(7), mux.pin(8),mux.pin(9)}, //array of pins
+ {60, CHANNEL_1}, //address of first button
+  1, //increment size
+  127, //velocity
+};
   
   // Initialize the Control Surface
 void setup() {
